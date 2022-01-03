34ea9f5eff2880125097005f5cb8cca1625975e5
Nguyen Duc Thao <thaond1205@gmail.com>
2022-01-03 21:47:09 +0700

    first commit

---
+++ microsevices-learn/src/main/java/com/microsevices/MicrosevicesLearnApplication.java
@@ -0,0 +1,13 @@
+package com.microsevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosevicesLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosevicesLearnApplication.class, args);
+	}
+
+}
